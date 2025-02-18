48a6f9f45ffbd9881c5160b9b1b703c063157e83
unknown <Bho.Groothoff@btc-ag.com>
2025-02-18 16:17:55 +0100

    works somewhat, now database path seems to be problem

--- src/main/java/com/btcag/bootcamp/DatabaseEntities/Robot.java
+++ src/main/java/com/btcag/bootcamp/DatabaseEntities/Robot.java
@@ -1,6 +1,6 @@
 package com.btcag.bootcamp.DatabaseEntities;
 
-import javax.persistence.Entity;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
